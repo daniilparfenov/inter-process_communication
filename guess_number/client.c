49d42e9512d33da738c465a35ee749ae12a86fbd
vadim <144333283+vadyukkha@users.noreply.github.com>
2024-11-19 20:45:45 +0300

    #add comments

--- guess_number/client.c
+++ guess_number/client.c
@@ -11,34 +11,50 @@
 #define BUFFER_SIZE 128
 
 int main() {
+    // Ожидание создания FIFO
     while (access(FIFO_PATH, F_OK) == -1) {
         printf("Waiting to start server...\n");
         sleep(1);
     }
+
     printf("Connected to server. Starting automatic guess...\n");
+
+    // Создаем дескриптор для записи
     file_descriptor_t fds_write;
     file_descriptor_t *ptr_fds_write = &fds_write;
+
+    // Создаем дескриптор для чтения
     file_descriptor_t fds_read;
     file_descriptor_t *ptr_fds_read = &fds_read;
-    int low = 1;
-    int high = 100;
-    int guess;
+
+    int low = 1;     // Нижняя граница диапазона
+    int high = 100;  // Верхняя граница диапазона
+    int guess;       // Текущее предположение
+
     while (1) {
-        guess = (low + high) / 2;
+        guess = (low + high) / 2;  // Определяем новое предположение (середина диапазона)
         printf("Sent: %d\n", guess);
+
+        // Преобразование числа в строку для отправки
         char guess_str[BUFFER_SIZE];
         snprintf(guess_str, BUFFER_SIZE, "%d", guess);
 
+        // Отправка числа серверу
         open_fds_connection(ptr_fds_write, FIFO_PATH, O_WRONLY);
         write_bytes_to_file(ptr_fds_write, guess_str);
         close_fds_connection(ptr_fds_write);
 
+        // Получение ответа от сервера
         open_fds_connection(ptr_fds_read, FIFO_PATH, O_RDONLY);
+
+        // Запись ответа от сервера в response
         char response[BUFFER_SIZE];
         ssize_t msg_size = read_bytes_from_file(ptr_fds_read, response);
         response[msg_size] = '\0';
         close_fds_connection(ptr_fds_read);
         printf("Received: %s\n\n", response);
+
+        // Анализ ответа сервера
         if (strcmp(response, "You guessed the number!") == 0) {
             printf("Client won! The number is %d.\n", guess);
             break;
--- guess_number/server.c
+++ guess_number/server.c
@@ -12,6 +12,7 @@
 #define FIFO_PATH "/tmp/fifo_file"
 #define BUFFER_SIZE 128
 
+// Обработчик сигнала
 void handle_signal(int sig) {
     if (sig == SIGINT) {
         printf("\nReceived SIGINT. Cleaning up...\n");
@@ -21,9 +22,12 @@ void handle_signal(int sig) {
 }
 
 int main() {
+    // Установка обработчика сигнала SIGINT
     signal(SIGINT, handle_signal);
 
     srand(time(NULL));
+
+    // Создание FIFO
     if (mkfifo(FIFO_PATH, 0666) == -1) {
         if (errno != EEXIST) {
             handle_error("mkfifo");
@@ -32,33 +36,39 @@ int main() {
 
     printf("Server started. Waiting for client...\n");
 
+    // Создаем дескриптор для чтения
     file_descriptor_t fds_read;
     file_descriptor_t *ptr_fds_read = &fds_read;
 
+    // Создаем дескриптор для записи
     file_descriptor_t fds_write;
     file_descriptor_t *ptr_fds_write = &fds_write;
 
     int guessed_number = rand() % 100 + 1;
 
-    char buffer[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];  // Буффер для обработки текста от пользователя
     size_t game_counter = 1;
     size_t game_attempt = 0;
 
     printf("\nGame number %zu\n\n", game_counter);
 
     while (1) {
+        // Открытие FIFO на чтение
         open_fds_connection(ptr_fds_read, FIFO_PATH, O_RDONLY);
         ++game_attempt;
 
+        // Чтение сообщения от клиента
         ssize_t msg_size = read_bytes_from_file(ptr_fds_read, buffer);
         close_fds_connection(ptr_fds_read);
 
-        buffer[msg_size] = '\0';
+        buffer[msg_size] = '\0';  // Нулевой символ в конце строки
         int estimated = atoi(buffer);
         printf("[Attempt %zu] << Received: %d\n", game_attempt, estimated);
 
+        // Ответ клиенту
         open_fds_connection(ptr_fds_write, FIFO_PATH, O_WRONLY);
 
+        // Анализ ответа пользователя
         const char *msg = "\n";
         if (estimated == guessed_number) {
             msg = "You guessed the number!";
@@ -69,8 +79,9 @@ int main() {
             msg = "The guessed number is more";
         }
 
-        sleep(3);
+        sleep(1);
 
+        // Отправка ответа пользователю
         write_bytes_to_file(ptr_fds_write, msg);
         close_fds_connection(ptr_fds_write);
         printf("[Attempt %zu] >> Sent: %s\n\n", game_attempt, msg);
@@ -80,6 +91,6 @@ int main() {
         }
     }
 
-    unlink(FIFO_PATH);
+    unlink(FIFO_PATH);  // Удаление FIFO
     return 0;
 }
